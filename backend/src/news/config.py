deed2333709d62fe1c908dd29fc72272baa41d35
RHappiness <a2a45a789@gmail.com>
2024-11-03 15:42:12 +0800

    refactor:將常數提出

--- backend/src/news/config.py
+++ backend/src/news/config.py
@@ -1,2 +1,3 @@
 GPT_MODEL="gpt-3.5-turbo"
 OPENAI_API_KEY="xxx"
+PAGES_INFO_URL="https://udn.com/api/more"
--- backend/src/users/router.py
+++ backend/src/users/router.py
@@ -4,6 +4,7 @@ from fastapi.security import OAuth2PasswordRequestForm, FastAPI
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from sqlalchemy.orm import Session
+from .constants import USER_ACCESS_EXPIRE_TIME
 from ..models import User
 from ..auth.services import check_user_password_is_correct, create_access_token 
 from ..models import User
@@ -15,16 +16,14 @@ app=FastAPI()
 async def login_for_access_token(
         form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(session_opener)
 ):
-    """login"""
     user = check_user_password_is_correct(db, form_data.username, form_data.password)
     access_token = create_access_token(
-        data={"sub": str(user.username)}, expires_delta=timedelta(minutes=30)
+        data={"sub": str(user.username)}, expires_delta=timedelta(minutes=USER_ACCESS_EXPIRE_TIME)
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
 @app.post("/api/v1/users/register")
 def create_user(user: UserAuthSchema, db: Session = Depends(session_opener)):
-    """create user"""
     hashed_password = pwd_context.hash(user.password)
     db_user = User(username=user.username, hashed_password=hashed_password)
     db.add(db_user)
